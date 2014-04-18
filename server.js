714a0762be44b86a784f86e0c200e9f5910fee3b
David Lusby <lusbyd@gmail.com>
2014-04-18 12:36:30 +0100

    Router configured and the app started

--- server.js
+++ server.js
@@ -6,3 +6,25 @@ var app = express();
 
 // configure to use body parser so we can get the data from a post
 app.use(bodyParser());
+
+var port = process.env.PORT || 8080; // set our port
+
+
+// ROUTES FOR OUR API
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.send('hooray! welcome to our api!');
+});
+
+// more routes will happen here
+
+
+// REGISTER OUR ROUTES
+// all our routes will be prefixed with /api
+app.use('/api', router);
+
+
+// START THE SERVER
+app.listen(port);
+console.log('Magic happens on port ' + port);
