75f758a59f9ebac04cdfb9bdb254da774e6a3cd5
David Lusby <lusbyd@gmail.com>
2014-04-18 12:49:20 +0100

    Added middleware to log the incoming request.
    This can be used for much more powerful work than this such as ensuring authenication,
    validating the request and other things. Middleware is called in the listed order in
    Express v4.0 - apparently this was an issue in prior versions.

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ var Bear = require('./app/models/bear');
 // ROUTES FOR OUR API
 var router = express.Router();
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+  // do logging
+  console.log('Something is happening.');
+  next(); // make sure we go to the next routes and don't stop here
+});
+
+// default API route
 router.get('/', function(req, res) {
   res.send('hooray! welcome to our api!');
 });
